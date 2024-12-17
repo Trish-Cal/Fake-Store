3acce8c0658e8a1e99127dd26d2fbba02802d81e
Trish-Cal <pcalfee@gmail.com>
2024-12-17 12:38:40 -0600

    my fifth commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import ProductList from '../components/ProductList/ProductList';
 import axios from 'axios'
+import "./Home.css"
 
 export default function Home({
   products, 
@@ -16,8 +17,42 @@ export default function Home({
     })
     .catch((err) => console.log(err));
     }, []);
+
+    const handleCategoryClick = (category)=>{
+
+    }
   return ( 
   <div>
+    <div className='filter-menu'>
+      <span>Filter by Category:</span>
+      <div className='filter-buttons'>
+        <p 
+        onClick={() => {
+          handleCategoryClick("men's clothing");
+          }}
+          >Men's Clothing</p>
+        <p 
+        onClick={() => {
+          handleCategoryClick("women's clothing");
+          }}
+          >Women's Clothing</p>
+        <p 
+        onClick={() => {
+          handleCategoryClick("electronics");
+          }}
+          >Electronics</p>
+        <p 
+        onClick={() => {
+          handleCategoryClick("jewelery");
+          }}
+          >Jewelry</p>
+        <p 
+        onClick={() => {
+          handleCategoryClick("");
+          }}
+          >Reset</p>
+      </div>
+    </div>
     <ProductList products={productsToDisplay}/>
   </div>
   );
